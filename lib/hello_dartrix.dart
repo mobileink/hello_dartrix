593ac789d77a116df1adf5f10f87c9972b9b19c0
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2020-04-01 13:54:16 -0500

    remove leftover var
    Signed-off-by: Gregg Reynolds <601396+mobileink@users.noreply.github.com>

--- lib/hello_dartrix.dart
+++ lib/hello_dartrix.dart
@@ -1,6 +1,6 @@
 import 'dart:isolate';
 
 // This is not used; it's only here to satisfy pub.dev
-void main(List<String> args, SendPort sport) {
-  dartrixPort.send({});
+void main(List<String> args, SendPort sPort) {
+  sPort.send({});
 }
