84ad8b7a45c083de86db53e54800355451eedb2a
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2020-04-01 13:52:13 -0500

    cleanup lib/hello_dartrix.dart
    Signed-off-by: Gregg Reynolds <601396+mobileink@users.noreply.github.com>

--- lib/src/hello.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:isolate';
-
-void handleDart(List<String> args, SendPort dartrixPort) async {
-  dartrixPort.send({
-    'lang': 'dart',
-    'dartrix': {'mergeData': true}
-  }); //  await generateFromBuiltin();
-}
